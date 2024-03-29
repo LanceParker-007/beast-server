88c2230e6bdc91803c584a0d45c40e3094602691
lance <lanceparker2001@gmail.com>
2024-03-29 22:58:00 +0530

    Node modules reinstalled

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { config } from "dotenv";
-import { connectDB } from "./config/db.js";
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
 
-config({
-  path: "./config/config.env",
-});
+let s3Client = null;
 
-export const s3Client = new S3Client({
-  region: "ap-south-1",
-  credentials: {
-    accessKeyId: process.env.AWS_IAM_USER_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_IAM_USER_SECRET_ACCESS_KEY,
-  },
-});
+try {
+  connectDB();
 
-connectDB();
+  s3Client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+      accessKeyId: process.env.AWS_IAM_USER_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_IAM_USER_SECRET_ACCESS_KEY,
+    },
+  });
 
-const server = app.listen(5000, () => {
-  console.log("Server is up and running!");
-});
+  app.listen(5000, () => {
+    console.log("Server is up and running!");
+  });
+} catch (error) {
+  console.log(error.message);
+}
 
-export { server };
+export { s3Client };
